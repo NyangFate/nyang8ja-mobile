a40d84a2f6a6af85b68cbfd52eb4fa229f8e7238
Jay Lee <roof2586@naver.com>
2025-02-02 20:56:02 +0900

    feat: RN 웹뷰 초기 설정

---
+++ components/Webview.tsx
@@ -0,0 +1,5 @@
+import { WebView as _WebView } from "react-native-webview";
+
+export default function WebView() {
+  return <_WebView source={{ uri: "https://tarotmeow.vercel.app/" }} style={{ flex: 1 }} />;
+}
