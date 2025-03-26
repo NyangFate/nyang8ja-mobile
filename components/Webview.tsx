deea08f2ae825b9432feea7a25169f27dd586ad8
Jay Lee <roof2586@naver.com>
2025-03-26 20:04:38 +0900

    chore: 프로젝트 파일 및 설정 리셋

--- components/Webview.tsx
+++
@@ -1,5 +0,0 @@
-import { WebView as _WebView } from "react-native-webview";
-
-export default function WebView() {
-  return <_WebView source={{ uri: "https://tarotmeow.vercel.app/" }} style={{ flex: 1 }} />;
-}
