801b43303d1bb18637283826fcf6a006d49c829e
Jay Lee <roof2586@naver.com>
2025-04-15 22:01:29 +0900

    feat: 마이페이지 컴포넌트에서 SVG 아이콘 이름 변경 및 네비게이션 로직 수정

--- app/mypage.tsx
+++ app/mypage.tsx
@@ -1,8 +1,8 @@
-import Bell from '@/assets/svgs/bell.svg';
-import Bookmark from '@/assets/svgs/bookmark.svg';
-import ChevronLeft from '@/assets/svgs/chevron-left.svg';
-import ChevronRight from '@/assets/svgs/chevron-right.svg';
-import CircleUser from '@/assets/svgs/circel-user.svg';
+import BellIcon from '@/assets/svgs/bell.svg';
+import BookmarkIcon from '@/assets/svgs/bookmark.svg';
+import ChevronLeftIcon from '@/assets/svgs/chevron-left.svg';
+import ChevronRightIcon from '@/assets/svgs/chevron-right.svg';
+import CircleUserIcon from '@/assets/svgs/circel-user.svg';
 import { useRouter } from 'expo-router';
 import React from 'react';
 import { Pressable, ScrollView, Text, View } from 'react-native';
@@ -16,8 +16,8 @@ export default function Mypage() {
       <SafeAreaView className="flex-1 bg-white">
         {/* 헤더 */}
         <View className="flex-row justify-between items-center p-[14px]">
-          <Pressable onPress={() => router.back()}>
-            <ChevronLeft width={24} height={24} />
+          <Pressable onPress={() => router.navigate('/')}>
+            <ChevronLeftIcon width={24} height={24} />
           </Pressable>
           <Text className="text-subhead3 font-suit-bold text-grey-90">마이</Text>
           <View className="w-6" />
@@ -25,17 +25,18 @@ export default function Mypage() {
 
         {/* 정보 카드 */}
         <View className="px-5 my-[12px]">
-          <View className="flex-row items-center justify-between py-4 pl-5 pr-4 bg-grey-00 rounded-xl">
-            <View className="gap-[2px]">
-              <Text className="text-headline1 text-grey-90 font-suit-bold">로그인하기</Text>
-              <Text className="text-body1 font-suit-regular text-grey-70">
-                로그인하고 맞춤 결과를 만나봐요
-              </Text>
+          <Pressable onPress={() => router.navigate('/login')}>
+            <View className="flex-row items-center justify-between py-4 pl-5 pr-4 bg-grey-00 rounded-xl">
+              <View className="gap-[2px]">
+                <Text className="text-headline1 text-grey-90 font-suit-bold">로그인하기</Text>
+                <Text className="text-body1 font-suit-regular text-grey-70">
+                  로그인하고 맞춤 결과를 만나봐요
+                </Text>
+              </View>
+
+              <ChevronRightIcon width={24} height={24} />
             </View>
-            <Pressable onPress={() => {}}>
-              <ChevronRight width={24} height={24} />
-            </Pressable>
-          </View>
+          </Pressable>
         </View>
 
         <ScrollView className="flex-1 my-2">
@@ -43,7 +44,7 @@ export default function Mypage() {
           <View className="px-5 py-3">
             <Text className="text-body1 text-grey-50 font-suit-regular">서비스</Text>
             <Pressable className="flex-row items-center gap-3 py-3">
-              <Bookmark width={24} height={24} />
+              <BookmarkIcon width={24} height={24} />
               <Text className="text-body3 font-suit-regular text-grey-90">결과 모아보기</Text>
             </Pressable>
           </View>
@@ -54,11 +55,11 @@ export default function Mypage() {
           <View className="px-5 py-3">
             <Text className="text-body1 text-grey-50 font-suit-regular">관리</Text>
             <Pressable className="flex-row items-center gap-3 py-3">
-              <CircleUser width={24} height={24} />
+              <CircleUserIcon width={24} height={24} />
               <Text className="text-body3 font-suit-regular text-grey-90">계정 관리</Text>
             </Pressable>
             <Pressable className="flex-row items-center gap-3 py-3">
-              <Bell width={24} height={24} />
+              <BellIcon width={24} height={24} />
               <Text className="text-body3 font-suit-regular text-grey-90">알림 설정</Text>
             </Pressable>
           </View>
