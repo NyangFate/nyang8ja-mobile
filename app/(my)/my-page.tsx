f0c5f585183cae00b013deacd816d3491349c236
Jay Lee <roof2586@naver.com>
2025-04-20 12:45:18 +0900

    feat: SafeAreaProvider 제거 및 SafeAreaView로 변경하여 코드 간소화

--- app/(my)/my-page.tsx
+++ app/(my)/my-page.tsx
@@ -4,22 +4,20 @@ import Menu from '@/entities/my/ui/menu';
 import { useRouter } from 'expo-router';
 import React from 'react';
 import { View } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function MyPage() {
   const router = useRouter();
 
   return (
-    <SafeAreaProvider>
-      <SafeAreaView className="flex-1 bg-white">
-        <Header />
-        <View className="my-3">
-          <InfoCard />
-        </View>
-        <View className="flex-1 my-2">
-          <Menu />
-        </View>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <SafeAreaView className="flex-1 bg-white">
+      <Header />
+      <View className="my-3">
+        <InfoCard />
+      </View>
+      <View className="flex-1 my-2">
+        <Menu />
+      </View>
+    </SafeAreaView>
   );
 }
--- app/login-page.tsx
+++ app/login-page.tsx
@@ -5,25 +5,23 @@ import LogoWitchCat from '@/entities/login/ui/LogoWitchCat';
 import { useRouter } from 'expo-router';
 import React from 'react';
 import { View } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function LoginPage() {
   const router = useRouter();
 
   return (
-    <SafeAreaProvider>
-      <SafeAreaView className="flex-1 bg-white">
-        <Header />
+    <SafeAreaView className="flex-1 bg-white">
+      <Header />
 
-        <View className="mt-16">
-          <LogoWitchCat />
-        </View>
+      <View className="mt-16">
+        <LogoWitchCat />
+      </View>
 
-        <View className="px-5 gap-3 mt-[54px]">
-          <KakaoLoginButton />
-          <AppleLoginButton />
-        </View>
-      </SafeAreaView>
-    </SafeAreaProvider>
+      <View className="px-5 gap-3 mt-[54px]">
+        <KakaoLoginButton />
+        <AppleLoginButton />
+      </View>
+    </SafeAreaView>
   );
 }
