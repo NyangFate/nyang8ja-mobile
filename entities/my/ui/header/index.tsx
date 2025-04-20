c886704dcc70c37125553622625977ada5cd817e
Jay Lee <roof2586@naver.com>
2025-04-20 12:45:49 +0900

    feat: expo-checkbox 패키지 추가 및 색상 유틸리티 파일 생성, 헤더 컴포넌트 리팩토링

--- entities/my/ui/header/index.tsx
+++ entities/my/ui/header/index.tsx
@@ -1,18 +1,13 @@
-import ChevronLeftIcon from '@/assets/svgs/chevron-left.svg';
+import BaseHeader from '@/shared/ui/base-header';
 import { useRouter } from 'expo-router';
 import React from 'react';
-import { Pressable, Text, View } from 'react-native';
 
 export default function Header() {
   const router = useRouter();
 
-  return (
-    <View className="flex-row justify-between items-center p-[14px]">
-      <Pressable onPress={() => router.navigate('/')}>
-        <ChevronLeftIcon width={24} height={24} />
-      </Pressable>
-      <Text className="text-subhead3 font-suit-bold text-grey-90">마이</Text>
-      <View className="w-6" />
-    </View>
-  );
+  const handleBackPress = () => {
+    router.navigate('/');
+  };
+
+  return <BaseHeader title="마이" onBackPress={handleBackPress} />;
 }
---
+++ shared/utils/colors.ts
@@ -0,0 +1,28 @@
+const COLORS = {
+  white: '#FFF',
+  black: '#000',
+  grey: {
+    '00': '#F6F8FA',
+    10: '#EAEEF2',
+    20: '#D0D7DE',
+    30: '#AFB8C1',
+    40: '#8C959F',
+    50: '#6E7781',
+    60: '#57606A',
+    70: '#434A53',
+    80: '#32383F',
+    90: '#24292F',
+  },
+  primary: {
+    '00': '#F6F5FF',
+    '01': '#DEDBFF',
+    '02': '#B4ACFF',
+    '03': '#7A6DF0',
+  },
+  error: '#ff4d4f',
+  warning: '#faad14',
+  success: '#52c41a',
+  link: '#46a6ff',
+} as const;
+
+export default COLORS;
