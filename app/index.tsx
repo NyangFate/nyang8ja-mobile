586ebaf89b18d73b36617ede68908b6dd051ca06
Jay Lee <roof2586@naver.com>
2025-04-23 22:13:29 +0900

    feat: 마이페이지 버튼 텍스트 수정 및 사주 정보 생성 버튼 텍스트 변경

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'expo-router';
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, View } from 'react-native';
 
 export default function Index() {
   const router = useRouter();
@@ -13,10 +13,9 @@ export default function Index() {
         alignItems: 'center',
       }}
     >
-      <Text>마이페이지</Text>
-      <Button title="홈으로 이동" onPress={() => router.push('/(my)/my-page')} />
+      <Button title="마이 페이지로 이동" onPress={() => router.push('/(my)/my-page')} />
       <Button
-        title="사주 정보 생성"
+        title="사주 정보 생성하기"
         onPress={() => router.push('/(my)/saju-info/(create)/create-page')}
       />
     </View>
