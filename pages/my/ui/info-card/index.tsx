f256a85e3c5af80d3f99f964e3c0b917e3e663d6
Jay Lee <roof2586@naver.com>
2025-05-03 12:54:15 +0900

    feat: 사용자 프로필 정보에 기반한 사주 정보 카드 컴포넌트 리팩토링

--- pages/my/ui/info-card/index.tsx
+++ pages/my/ui/info-card/index.tsx
@@ -1,58 +1,58 @@
 import ChillSwagSunglassesCatImage from '@/assets/images/chill-swag-sunglasses-cat.webp';
 import ChevronRightIcon from '@/assets/svgs/chevron-right.svg';
+import { UserProfileResponseDto, UserResponseDto } from '@/openapi/models';
+import useUser from '@/pages/my/api/useUser';
 import cn from '@/shared/utils/cn';
 import dayjs from '@/shared/utils/dayjs';
 import { Image } from 'expo-image';
 import { useRouter } from 'expo-router';
 import React from 'react';
 import { Pressable, Text, View } from 'react-native';
-import { SajuInfo } from '../../model/types';
 
 export default function InfoCard() {
   const router = useRouter();
+  const { data: user } = useUser();
 
-  const user = undefined;
-
-  const handleNavigate = () => {
+  const handleNavigate = (user: UserResponseDto | undefined) => {
     if (!user) {
       router.navigate('/login-page');
-    } else if (user.sajuInfo) {
+    } else if (user.profile) {
       router.navigate('/saju-info/edit-page');
     } else {
       router.navigate('/saju-info/create-page');
     }
   };
 
-  const getUserDescription = () => {
+  const getUserDescription = (user: UserResponseDto | undefined) => {
     if (!user) {
       return '로그인하고 맞춤 결과를 만나봐요';
     }
 
-    if (user.sajuInfo) {
-      return formatSajuInfoDescription(user.sajuInfo);
+    if (user.profile) {
+      return formatSajuInfoDescription(user.profile);
     }
 
     return '사주 정보를 입력해 주세요';
   };
 
-  const formatSajuInfoDescription = (sajuInfo: SajuInfo) => {
-    const gender = sajuInfo.gender === 'female' ? '여' : '남';
-    const birthDate = dayjs.tz(sajuInfo.birthDate, 'Asia/Seoul').format('YYYY.MM.DD');
+  const formatSajuInfoDescription = (profile: UserProfileResponseDto) => {
+    const gender = profile.gender === 'FEMALE' ? '여' : '남';
+    const birthDate = dayjs.tz(profile.birthday, 'Asia/Seoul').format('YYYY.MM.DD');
     const separator = ' ・ ';
 
     let birthTimeInfo = '모름';
-    if (sajuInfo.isBirthTimeKnown) {
-      birthTimeInfo = dayjs.tz(sajuInfo.birthDate, 'Asia/Seoul').format('HH시 mm분');
+    if (profile.birthtime) {
+      birthTimeInfo = dayjs.tz(profile.birthtime, 'Asia/Seoul').format('HH시 mm분');
     }
 
     return `${gender}${separator}${birthDate}${separator}${birthTimeInfo}`;
   };
 
-  const description = getUserDescription();
+  const description = getUserDescription(user);
 
   return (
     <View className="px-5">
-      <Pressable onPress={handleNavigate}>
+      <Pressable onPress={() => handleNavigate(user)}>
         <View className="flex-row items-center justify-between py-4 pl-5 pr-4 bg-grey-00 rounded-xl">
           <View className="flex-row items-center gap-[10px]">
             {user && (
@@ -60,7 +60,7 @@ export default function InfoCard() {
                 className={cn(
                   'bg-white border rounded-full border-grey-20 px-[6px] py-2 w-[60px] h-[60px] justify-center items-center',
                   {
-                    'opacity-35': !user.sajuInfo,
+                    'opacity-35': !user.profile,
                   }
                 )}
               >
@@ -69,7 +69,7 @@ export default function InfoCard() {
             )}
             <View className="gap-[2px]">
               <Text className="text-headline1 text-grey-90 font-suit-bold">
-                {user ? user.name : '로그인하기'}
+                {user ? user.profile?.name : '로그인하기'}
               </Text>
               <Text className="text-body1 font-suit-regular text-grey-70">{description}</Text>
             </View>
