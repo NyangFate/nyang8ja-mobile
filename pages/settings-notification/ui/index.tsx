7d0d81c9acd1e824e871164f3afaf65adaed04b5
Jay Lee <roof2586@naver.com>
2025-06-03 18:42:01 +0900

    feat: 알림 설정에서 'SERVICE' 섹션 필터링 추가 및 코드 정리

--- pages/settings-notification/ui/index.tsx
+++ pages/settings-notification/ui/index.tsx
@@ -42,31 +42,33 @@ export default function Notification() {
     <SafeAreaView className="flex-1 bg-white">
       <Header title="알림 설정" onBackPress={handleBackPress} />
 
-      {notification.map((section, index) => (
-        <React.Fragment key={section.key}>
-          <View className="px-5 py-3">
-            <Text className="text-body1 text-grey-50 font-suit-regular">{section.label}</Text>
-            {section.items.map((item) => (
-              <View className="flex-row items-center justify-between py-3" key={item.key}>
-                <Text className="text-body3 text-grey-70 font-suit-regular">{item.label}</Text>
-                <Switch
-                  trackColor={{ false: COLORS.grey[10], true: COLORS.primary['03'] }}
-                  thumbColor={COLORS.white}
-                  onValueChange={() =>
-                    handleValueChange({
-                      value: !item.enabled,
-                      key: item.key,
-                    })
-                  }
-                  value={item.enabled}
-                />
-              </View>
-            ))}
-          </View>
-          {/* 구분선 */}
-          {index !== notification.length - 1 && <View className="h-3 bg-grey-10" />}
-        </React.Fragment>
-      ))}
+      {notification
+        .filter((section) => section.key !== 'SERVICE')
+        .map((section, index) => (
+          <React.Fragment key={section.key}>
+            <View className="px-5 py-3">
+              <Text className="text-body1 text-grey-50 font-suit-regular">{section.label}</Text>
+              {section.items.map((item) => (
+                <View className="flex-row items-center justify-between py-3" key={item.key}>
+                  <Text className="text-body3 text-grey-70 font-suit-regular">{item.label}</Text>
+                  <Switch
+                    trackColor={{ false: COLORS.grey[10], true: COLORS.primary['03'] }}
+                    thumbColor={COLORS.white}
+                    onValueChange={() =>
+                      handleValueChange({
+                        value: !item.enabled,
+                        key: item.key,
+                      })
+                    }
+                    value={item.enabled}
+                  />
+                </View>
+              ))}
+            </View>
+            {/* 구분선 */}
+            {/* {index !== notification.length - 1 && <View className="h-3 bg-grey-10" />} */}
+          </React.Fragment>
+        ))}
     </SafeAreaView>
   );
 }
